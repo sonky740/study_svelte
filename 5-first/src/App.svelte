79a3c2333ee3bc36b51858e979e49d8cdbfbafe2
Sonky <sonky740@gmail.com>
2022-11-28 00:20:32 +0900

    26~~

--- 5-first/src/App.svelte
+++ 5-first/src/App.svelte
@@ -1,16 +1,45 @@
 <script>
-  export let name;
-  export let age;
+  import ContactCard from './ContactCard.svelte';
 
-  $: console.log(name, age);
+  let name = 'Max';
+  let title = 'Job';
+  let image =
+    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg';
+  let description = 'A short <strong>description</strong>';
+  let age = 30;
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === 'Manuel') {
+    console.log('It runs!');
+    age = 31;
+  }
 
   function incrementAge() {
     age++;
   }
+
+  function changeName() {
+    name = 'Manuel';
+  }
+
+  function nameInput(e) {
+    name = e.target.value;
+  }
 </script>
 
-<h1>Hello {name}!, my age is {age}</h1>
+<h1>Hello {uppercaseName}!, my age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
+<input type="text" bind:value={title} />
+<input type="text" bind:value={image} />
+<textarea rows="3" bind:value={description} />
+
+<ContactCard userName={name} jobTitle={title} userImage={image} {description} />
 
 <style>
   h1 {
