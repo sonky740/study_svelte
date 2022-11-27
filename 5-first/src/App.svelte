236236651d017a4138ca7c5421cd11d898c5802c
Sonky <sonky740@gmail.com>
2022-11-27 21:23:07 +0900

    learn svelte

---
+++ 5-first/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let name;
+  export let age;
+
+  $: console.log(name, age);
+
+  function incrementAge() {
+    age++;
+  }
+</script>
+
+<h1>Hello {name}!, my age is {age}</h1>
+<button on:click={incrementAge}>Change Age</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
