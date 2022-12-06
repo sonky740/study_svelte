229aa223a9efe2bc701778668770abb317328168
Sonky <sonky740@gmail.com>
2022-12-07 00:02:53 +0900

    109~~

---
+++ 101-store/src/Cart/Cart.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { onDestroy } from 'svelte';
+  import cartItems from '../store/cart';
+  import CartItem from './CartItem.svelte';
+
+  // $변수를 쓰면 아래 함수를 알아서 호출해줌.
+
+  // let items;
+  // const unsubscribe = cartItems.subscribe((its) => {
+  //   items = its;
+  // }); // 데이터가 바뀔 때 실행하는 함수
+
+  // onDestroy(() => {
+  //   if (unsubscribe) {
+  //     unsubscribe();
+  //   }
+  // }); // 컴포넌트가 파괴될 때 실행하는 함수
+</script>
+
+<section>
+  <h1>Cart</h1>
+  <ul>
+    {#each $cartItems as item (item.id)}
+      <CartItem id={item.id} title={item.title} price={item.price} />
+    {:else}
+      <p>No items in cart yet!</p>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+    border-bottom: 2px solid #ccc;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ 101-store/src/Products/Products.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { products } from '../store/product';
+  import Product from '../Products/Product.svelte';
+</script>
+
+<section>
+  <h1>Products</h1>
+  {#each $products as product (product.id)}
+    <Product
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      description={product.description}
+    />
+  {/each}
+</section>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+  }
+</style>
